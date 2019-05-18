0802302fda16dc60ee44ec9bfc807a245fd577cc
Djafar Ould slimane <djafarouldslimane@Djafars-MBP.home>
2019-05-17 23:12:52 -0400

    aa

---
+++ src/Hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package Hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name1;
+        String name2;
+        String name3;
+
+        System.out.print("Name1, ");
+        System.out.print("Name3");
+        System.out.print(" and Name2");
+
+
+
+    }
+}
---
+++ src/Hello/ScannerPractice.java
@@ -0,0 +1,16 @@
+package Hello;
+
+import java.util.ArrayList;
+
+public class ScannerPractice {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        Scanner scanner = new Scaner()
+
+    }
+
+
+}
